b6a69321e3ba5d13a732a237d984ef0a2baf383b
MehediHasan23 <developer.mehedi23@gmail.com>
2023-07-23 20:05:52 +0600

    fix: profile form

--- client/src/components/Form/ProfileForm.jsx
+++ client/src/components/Form/ProfileForm.jsx
@@ -1,76 +1,89 @@
 /* eslint-disable react/prop-types */
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import avatar from "./../../assets/userImg.png";
+import defaultAvatar from "./../../assets/userImg.png";
 import TextInput from "./TextInput";
 
 // eslint-disable-next-line react/prop-types
 const ProfileForm = () => {
-  return (
-    <div className="bg-white dark:bg-boxDark p-8 rounded-lg shadow-md col-span-2">
-      <h2 className="dark:border-gray-500 text-2xl border-[#dedede] border-b-2 py-3 font-semibold text-[#344767] dark:text-white mb-8">
-        Update Profile
-      </h2>
-      <form encType="multipart/form-data">
-        <div className="grid grid-cols-12 gap-x-4">
-          {/* Upload Image  */}
-          <div className="col-span-12 mb-4">
-            <div className="flex items-start gap-x-4">
-              <div className="relative w-28 h-28 overflow-hidden flex items-center border-2 mb-5 rounded-full">
-                <label
-                  htmlFor="image"
-                  className="absolute cursor-pointer text-gray-400 right-1 left-1 text-center"
-                >
-                </label>
-                <input hidden id="image" name="image" type="file" />
+  const auth = useSelector((state) => state.auth);
+  const { name, email, address, avatar, phone } = auth.userInfo;
 
-                <img src={avatar} alt="Avatar" />
+  return (
+    <div className='bg-white dark:bg-boxDark p-8 rounded-lg shadow-md col-span-2'>
+      <h2 className='dark:border-gray-500 text-2xl border-[#dedede] border-b-2 py-3 font-semibold text-[#344767] dark:text-white mb-8'>
+        {name}
+      </h2>
+      <form encType='multipart/form-data'>
+        <div className='grid grid-cols-12 gap-x-4'>
+          {/* Upload Image  */}
+          <div className='col-span-12 mb-4'>
+            <div className='flex items-start gap-x-4'>
+              <div className='relative w-28 h-28 overflow-hidden flex items-center border-2 mb-5 rounded-full'>
+                <label
+                  htmlFor='image'
+                  className='absolute cursor-pointer text-gray-400 right-1 left-1 text-center'></label>
+                <input hidden id='image' name='image' type='file' />
+
+                <img src={avatar || defaultAvatar} alt='Avatar' />
               </div>
 
               <div>
-                <p className="text-[22px] font-medium">Dip</p>
-                <p className="text-[15px] font-normal">Supper Admin</p>
+                <p className='text-[22px] font-medium'>Dip</p>
+                <p className='text-[15px] font-normal'>Supper Admin</p>
               </div>
             </div>
 
             {/* Edit Profile Button */}
-            <Link to="update-profile">
-              <button className="bg-[#cbd5e1] border-none outline-none px-8 py-2 rounded-xl text-gray-800 font-medium">
+            <Link to='update-profile'>
+              <button className='bg-[#cbd5e1] border-none outline-none px-8 py-2 rounded-xl text-gray-800 font-medium'>
                 Edit Profile
               </button>
             </Link>
           </div>
 
           {/* Name  */}
-          <div className="col-span-6">
-            <TextInput title="name" type="text" defaultValue="Dip" />
+          <div className='col-span-6'>
+            <TextInput title='name' type='text' defaultValue={name} readOnly />
           </div>
 
           {/* Email */}
-          <div className="col-span-6">
-            <TextInput title="email" type="email" defaultValue="Dip@.com" />
-          </div>
-
-          {/* Password */}
-          <div className="col-span-6">
+          <div className='col-span-6'>
             <TextInput
-              title="password"
-              type="password"
-              defaultValue="1234567"
+              title='email'
+              type='email'
+              defaultValue={email}
+              readOnly
             />
           </div>
 
-          {/* Mobile */}
-          <div className="col-span-6">
+          {/* Password
+          <div className='col-span-6'>
             <TextInput
-              title="number"
-              type="number"
-              defaultValue="01721345453"
+              title='password'
+              type='password'
+              defaultValue='1234567'
+            />
+          </div> */}
+
+          {/* Mobile */}
+          <div className='col-span-6'>
+            <TextInput
+              title='number'
+              type='number'
+              defaultValue={phone || ""}
+              readOnly
             />
           </div>
 
           {/* Address */}
-          <div className="col-span-6">
-            <TextInput title="address" type="text" defaultValue="Sylhet" />
+          <div className='col-span-6'>
+            <TextInput
+              title='address'
+              type='text'
+              defaultValue={address || ""}
+              readOnly
+            />
           </div>
         </div>
       </form>
--- client/src/components/Form/UpdateProfileForm.jsx
+++ client/src/components/Form/UpdateProfileForm.jsx
@@ -5,68 +5,61 @@ import TextInput from "./TextInput";
 // eslint-disable-next-line react/prop-types
 const UserProfileForm = () => {
   return (
-    <div className="bg-white dark:bg-boxDark p-8 rounded-lg shadow-md col-span-2">
-      <h2 className="dark:border-gray-500 text-2xl border-[#dedede] border-b-2 py-3 font-semibold text-[#344767] dark:text-white mb-8">
+    <div className='bg-white dark:bg-boxDark p-8 rounded-lg shadow-md col-span-2'>
+      <h2 className='dark:border-gray-500 text-2xl border-[#dedede] border-b-2 py-3 font-semibold text-[#344767] dark:text-white mb-8'>
         Update Profile
       </h2>
-      <form encType="multipart/form-data">
-        <div className="grid grid-cols-12 gap-x-4">
+      <form encType='multipart/form-data'>
+        <div className='grid grid-cols-12 gap-x-4'>
           {/* Upload Image  */}
-          <div className="col-span-12 mb-4">
-            <div className="flex items-start gap-x-4">
-              <div className="relative w-28 h-28 overflow-hidden flex items-center border-2 mb-5 rounded-full">
+          <div className='col-span-12 mb-4'>
+            <div className='flex items-start gap-x-4'>
+              <div className='relative w-28 h-28 overflow-hidden flex items-center border-2 mb-5 rounded-full'>
                 <label
-                  htmlFor="image"
-                  className="absolute cursor-pointer text-gray-400 right-1 left-1 text-center"
-                >
-                  <AiOutlineCamera className="mx-auto text-2xl" />
+                  htmlFor='image'
+                  className='absolute cursor-pointer text-gray-400 right-1 left-1 text-center'>
+                  <AiOutlineCamera className='mx-auto text-2xl' />
                 </label>
-                <input hidden id="image" name="image" type="file" />
+                <input hidden id='image' name='image' type='file' />
               </div>
 
               <div>
-                <p className="text-[22px] font-medium">Dip</p>
-                <p className="text-[15px] font-normal">Supper Admin</p>
+                <p className='text-[22px] font-medium'>Dip</p>
+                <p className='text-[15px] font-normal'>Supper Admin</p>
               </div>
             </div>
           </div>
 
           {/* Name  */}
-          <div className="col-span-6">
-            <TextInput title="name" type="text" defaultValue="Dip" />
+          <div className='col-span-6'>
+            <TextInput title='name' type='text' />
           </div>
 
           {/* Email */}
-          <div className="col-span-6">
-            <TextInput title="email" type="email" defaultValue="Dip@gmail.com" />
+          <div className='col-span-6'>
+            <TextInput title='email' type='email' />
           </div>
 
           {/* Password */}
-          <div className="col-span-6">
-            <TextInput
-              title="password"
-              type="password"
-              defaultValue="1234567"
-            />
+          <div className='col-span-6'>
+            <TextInput title='password' type='password' />
           </div>
 
           {/* Mobile */}
-          <div className="col-span-6">
-            <TextInput
-              title="number"
-              type="number"
-              defaultValue="01721345453"
-            />
+          <div className='col-span-6'>
+            <TextInput title='number' type='number' />
           </div>
 
           {/* Address */}
-          <div className="col-span-6">
-            <TextInput title="address" type="text" defaultValue="Sylhet" />
+          <div className='col-span-6'>
+            <TextInput title='address' type='text' defaultValue='Sylhet' />
           </div>
 
           {/* Update Form Btn */}
-          <div className="col-span-12 text-end">
-            <button className="bg-indigo-500 hover:bg-indigo-600 text-base text-white font-medium py-2 mt-4 w-1/3 rounded focus:outline-none focus:shadow-outline mx-auto spinner-button">Update</button>
+          <div className='col-span-12 text-end'>
+            <button className='bg-indigo-500 hover:bg-indigo-600 text-base text-white font-medium py-2 mt-4 w-1/3 rounded focus:outline-none focus:shadow-outline mx-auto spinner-button'>
+              Update
+            </button>
           </div>
         </div>
       </form>
